0c67fedf6231fc721b40a56760a531242db6df67
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 17:01:00 +0530

    new lines

---
+++ src/org/gitsample/SampleGit.java
@@ -0,0 +1,11 @@
+package org.gitsample;
+
+public class SampleGit {
+	
+
+	public static void main(String[] args) {
+		System.out.println("thiru");
+		System.out.println("thirumalai");
+		System.out.println("thirumalairajan");
+	}
+}
