5cec6a93403f4a9a1aa3da6ce014b95d9053da6c
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 17:12:49 +0530

    lines

--- src/org/gitsample/SampleGit.java
+++ src/org/gitsample/SampleGit.java
@@ -1,7 +1,9 @@
 package org.gitsample;
 
 public class SampleGit {
-	
+	private void t1() {
+System.out.println("thiru1 end of day");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("thiru");
